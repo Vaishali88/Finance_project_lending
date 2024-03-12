602f57d8eed91a4b896c4535682ad18b51d48a92
Your Name <you@example.com>
2024-03-12 07:25:54 +0530

    first

---
+++ customer_data.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkContext, SparkConf
+from pyspark.sql.functions import col, current_timestamp
+
+conf_spark = SparkConf().set("spark.driver.host", "127.0.0.1")
+spark = SparkSession.builder\
+			.appName('demo01')\
+			.config('spark.sql.shuffle.partitions', '4') \
+	        .master('local') \
+	        .getOrCreate()
+
+filepath = "hdfs://localhost:9000/lending_club_project/customer_data/part-00000-694b65c5-0b8f-4760-ac12-2b59978292e0-c000.csv"
+
+scschema = ("member_id string,emp_title string,emp_length string,home_ownership string,annual_inc float,addr_state string,zip_code string,country string,grade string,sub_grade string,verification_status string,tot_hi_cred_lim float,application_type string,annual_inc_joint float,verification_status_joint string")
+df = spark.read.format("csv") \
+    .option("inferSchema",True) \
+	.option("header",True) \
+	.schema(scschema)\
+    .load(filepath)
+
+
+df = df.withColumnRenamed('annual_inc',"annual_income") \
+	.withColumnRenamed('addr_state','address_state')\
+	.withColumnRenamed('zip_code','address_zip_code')\
+	.withColumnRenamed('country','address_country')\
+	.withColumnRenamed('tot_hi_cred_lim','total_high_credit_limit')\
+	.withColumnRenamed('annual_inc_joint','join_annual_income')
+df.printSchema()
+customer_df_renamed = df.withColumn("ingest_date",current_timestamp())
+customer_df_renamed.printSchema()
+print(customer_df_renamed.count())
+customer_distinct = customer_df_renamed.distinct().count()
+print(customer_distinct)
---
+++ pq.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkContext, SparkConf
+
+conf_spark = SparkConf().set("spark.driver.host", "127.0.0.1")
+spark = SparkSession.builder\
+			.appName('demo01')\
+			.config('spark.sql.shuffle.partitions', '4') \
+	        .master('local') \
+	        .getOrCreate()
+filepath = "hdfs://localhost:9000/lending_club_project/accepted_2007_to_2018Q4.csv"
+df = spark.read \
+	.format("csv") \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+	.load()
+df.printSchema()
